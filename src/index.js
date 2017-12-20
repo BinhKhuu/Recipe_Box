70e897f37f998bb579276ae5ac8d79ca8282741b
spoonable <binhkhuu90@gmail.com>
2017-12-20 17:21:27 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class RecipyData extends React.Component {
-
+	render() {
+		return (
+			<tr className='recipy-data' style={{display:this.props.display}}><td><a className='accordian-toggle' data-toggle='collapse'>hi</a></td></tr>
+		)
+	}
 }
 
 class RecipyListItem extends React.Component {
 	render() {
 		return (
-			<tr className ='recipy-data' onClick={this.props.show}>
-				<td>{this.props.title}</td>
+			<tr>
+				<td className ='list-item' onClick={this.props.show}>{this.props.title}</td>
 				<td><button onClick={this.props.remove}>delete</button></td>
 			</tr>
 		)
@@ -22,6 +26,8 @@ class RecipyList extends React.Component {
 		super();
 		this.state = {
 			recipyList: ['o hai', 'mark'],
+			recipyData: ['none','none']
+			//can have array that tracks which items are hidden and when are dispalyed for the RecipyData
 		}
 		this.removeRecipy = this.removeRecipy.bind(this);
 		this.addRecipy = this.addRecipy.bind(this);
@@ -30,8 +36,8 @@ class RecipyList extends React.Component {
 	addRecipy(index) {
 		var rows = this.state.recipyList
 		rows.push('new');
+		//add recipydata array aswell
 		this.setState({recipyList:rows })
-		console.log(this.state.recipyList);
 	}
 	removeRecipy(index) {
 		var list = this.state.recipyList;
@@ -39,7 +45,11 @@ class RecipyList extends React.Component {
 		this.setState({recipyList: list});		
 	}
 	showRecipy(index) {
-		console.log(index)
+		var display = this.state.recipyData;
+		var toggle = (display[index] === 'none') ? '' : 'none';
+		display.splice(index,1);
+		display.splice(index,0,toggle);
+		this.setState({recipyData:display});
 	}
 	render() {
 		return (
@@ -48,13 +58,16 @@ class RecipyList extends React.Component {
 					<thead>
 						<tr className='thead-light'>
 							<th scope='col' width='90%' >Recipy</th>
-							<th scop='col' width='10%'></th>
+							<th scope='col' width='10%'></th>
 						</tr>
 					</thead>
 					<tbody>
 						{this.state.recipyList.map((x,i)=> {
-							return <RecipyListItem className='recipy-data' show={this.showRecipy.bind(this, i)} key={i} title={this.state.recipyList[i]} remove={this.removeRecipy.bind(this, i)} />
-						})}					
+							return ([
+								<RecipyListItem className='recipy-data' show={this.showRecipy.bind(this, i)} key={i} title={this.state.recipyList[i]} remove={this.removeRecipy.bind(this, i)} />,
+								<RecipyData key={'row-expanded-' + i} display={this.state.recipyData[i]}/>
+								])
+						})}				
 					</tbody>
 				</table>
 				<button onClick={this.addRecipy}>add</button>
